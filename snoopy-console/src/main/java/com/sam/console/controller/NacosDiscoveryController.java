bbe1af66163878ef7c83b2b1e46fed71cd4f796a
MaShaoXu <654299054@qq.com>
2020-09-30 14:22:11 +0800

    init

--- snoopy-console/src/main/java/com/sam/console/controller/NacosDiscoveryController.java
+++ snoopy-console/src/main/java/com/sam/console/controller/NacosDiscoveryController.java
@@ -1,18 +1,14 @@
 package com.sam.console.controller;
 
 import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
-import com.alibaba.nacos.api.exception.NacosException;
 import com.alibaba.nacos.api.naming.NamingMaintainService;
 import com.alibaba.nacos.api.naming.NamingService;
-import com.alibaba.nacos.api.naming.pojo.Instance;
-import com.alibaba.nacos.api.naming.pojo.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.PostConstruct;
-import java.util.List;
 
 @RestController
 @RequestMapping("/nacos/discovery")
@@ -29,26 +25,9 @@ public class NacosDiscoveryController {
         namingMaintainService = properties.namingMaintainServiceInstance();
     }
 
-    @GetMapping("/testMaintain")
-    public String queryService(String serviceName){
-        try {
-            Service service = namingMaintainService.queryService(serviceName);
-
-            return "OK";
-        } catch (NacosException e) {
-            e.printStackTrace();
-            return "ERR";
-        }
-    }
-
-    @GetMapping("/getAllInstances")
-    public List<Instance> getAllInstances(String serviceName){
-        try {
-            return namingService.getAllInstances(serviceName);
-        } catch (NacosException e) {
-            e.printStackTrace();
-            return null;
-        }
+    @GetMapping("/serverStatus")
+    public String serverStatus() {
+        return namingService.getServerStatus();
     }
 
 }
