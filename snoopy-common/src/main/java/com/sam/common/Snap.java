dd80407c0a98c2b76ae0ac4b90f6abf000640897
MaShaoXu <654299054@qq.com>
2020-10-07 18:45:24 +0800

    init

---
+++ snoopy-common/src/main/java/com/sam/common/Snap.java
@@ -0,0 +1,30 @@
+package com.sam.common;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+public class Snap {
+    private static Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
+
+    /**
+     * 对屏幕进行拍照
+     **/
+    public static void snapshot(String filename, String format) {
+        try {
+            Robot robot = new Robot();
+            //拷贝屏幕到一个BufferedImage对象screenshot
+            BufferedImage screenshot = robot.createScreenCapture(new Rectangle(0, 0, (int) d.getWidth(), (int) d.getHeight()));
+            ImageIO.write(screenshot, format, new File(filename + "." + format));
+            System.out.println("..Finished");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        snapshot("C:\\Users\\Sam\\Desktop\\日志\\java\\bootstrap栅格", "png");
+    }
+
+}
