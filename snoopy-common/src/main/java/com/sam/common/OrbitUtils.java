a72b5481d3c48c0944c610e9d40655a11a296975
MaShaoXu <654299054@qq.com>
2020-10-28 20:49:15 +0800

    init

---
+++ snoopy-common/src/main/java/com/sam/common/OrbitUtils.java
@@ -0,0 +1,22 @@
+package com.sam.common;
+
+public class OrbitUtils {
+
+    private static final double PI = Math.PI;
+    private static final double G = 6.67428e-11;
+    private static final double M = 5.965e+24;
+    private static final double GM = G * M;
+
+    public static Double exe(int t) {
+        double sqrt = Math.pow(t, 2);
+        double pi4 = Math.pow(PI, 2) * 4;
+        double result = sqrt * GM / pi4;
+        return Math.pow(result, 1.0/3.0);
+    }
+
+    public static void main(String[] args) {
+        Double exe = exe(86635);
+        System.out.println(exe);
+    }
+
+}
