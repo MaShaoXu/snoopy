54c80114845cde6a404f0bc295f694f8bfde62fd
MaShaoXu <654299054@qq.com>
2020-11-05 15:42:31 +0800

    init

--- snoopy-common/src/main/java/com/sam/common/OrbitUtils.java
+++ snoopy-common/src/main/java/com/sam/common/OrbitUtils.java
@@ -7,17 +7,27 @@ public class OrbitUtils {
     private static final double M = 5.965e+24;
     private static final double GM = G * M;
 
-    public static Double exe(int t) {
+    public static Double testa(int t) {
         double sqrt = Math.pow(t, 2);
         double pi4 = Math.pow(PI, 2) * 4;
         double result = sqrt * GM / pi4;
         return Math.pow(result, 1.0/3.0);
     }
 
+    public static Double testb(Double a, Double e){
+        double c = a * e;
+        double a2 = Math.pow(a, 2);
+        double c2 = Math.pow(c, 2);
+        return Math.pow((a2-c2), 1.0/2.0);
+    }
+
     public static void main(String[] args) {
         int d = 82800 + 3364;
-        Double exe = exe(d);
-        System.out.println(exe);
+        double c = 0.00001;
+        Double a = testa(d);
+        System.out.println(a);
+        Double b = testb(a, c);
+        System.out.println(b);
     }
 
 }
