c0c79a973e77828b102ee3ab1ff8520ce12a034d
MaShaoXu <654299054@qq.com>
2020-11-11 17:41:08 +0800

    init

---
+++ snoopy-oauth2/src/main/java/com/sam/oauth2/config/OAuth2AuthServerConfig.java
@@ -0,0 +1,24 @@
+package com.sam.oauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+@Configuration
+public class OAuth2AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory() // 使用in-memory存储
+                .withClient("client") // client_id
+                .secret("secret") // client_secret
+                .authorizedGrantTypes("authorization_code") // 该client允许的授权类型
+                .scopes("app"); // 允许的授权范围
+    }
+
+}
+
